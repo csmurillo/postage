6324eaab1732383a97e75aa392bbf1f0dcbab5be
csmurillo <csmurillo00@gmail.com>
2022-12-03 21:20:15 -0800

    migrations

--- database/migrations/2022_06_27_220110_create_profiles_table.php
+++ database/migrations/2022_06_27_220110_create_profiles_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->text('description')->default('');
+            $table->text('description');
+            // $table->text('description')->default('');
             $table->timestamps();
             $table->index('user_id');
         });
